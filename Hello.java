08bbdef219546bb643e081ddbeac324437310f2f
shlomi hemo <shlomihemo82@gmail.com>
2025-03-24 11:40:02 +0200

    Hello.java update after changes !

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+    public static void main(String[] arg){
+        for(int i=1;i<=10;i++){
+            System.out.println("moshe this is the print aftert the changes ! ..."+i);
+        }
+    }
+}
